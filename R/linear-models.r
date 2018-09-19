931351c7fc4e64854020f3b663db6184367bcb0b
Xiangyi Shan <shawnieshan@Xiangyis-MBP.wireless.yale.internal>
2018-09-19 11:16:24 -0400

    HW1

--- R/linear-models.r
+++ R/linear-models.r
@@ -10,6 +10,21 @@
 #' fit <- linear_model(Sepal.Length ~., iris)
 #' summary(fit)
 #' @export
+data(iris)
+View(data)
 linear_model <- function(formula, data) {
-  lm(formula, data)
+  #browser()
+  mm<-model.matrix(formula, data)
+  y<-data$Sepal.Length[as.integer(row.names(mm))]
+  x<-mm[,2:6]
+  svd_output <- svd(x)
+  U <- svd_output[["u"]]
+  Sinv <- diag(1 / svd_output[["d"]])
+  V <- svd_output[["v"]]
+  pseudo_inv <- V %*% Sinv %*% t(U)
+  betahat <- pseudo_inv %*% y
+  colnames(betahat) <- "regression coefficient"
+  rownames(betahat) <- c("Sepal.Width","Patal.Length","Petal.Width","Species_versicolor","Species_virginica")
+  print(betahat)
+#lm(formula, data)
 }

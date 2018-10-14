34885716e6911bec945038d3fe4f14abc2f8b037
Xiangyi Shan <shawnieshan@Xiangyis-MBP.wireless.yale.internal>
2018-10-14 12:19:00 -0400

    homework2

--- R/ridge_model.R
+++ R/ridge_model.R
@@ -1,17 +1,17 @@
 #' Fit a ridge regression model
 #'
 #' @description This function passes parameters to the ridge regression function.
-#' @param form a formula
+#' @param formula a formula
 #' @param lambda a hyper parameter
 #' @param data a data.frame
 #' @return An ridge regression object
 #' @export
 
 #build ridge function
-ridge_reg<-function(form, lambda, data){
+ridge_reg<-function(formula, lambda, data){
   rownames(data) = NULL
-  m<-model.matrix(form, data)
-  y<-matrix(data[,as.character(form)[2]],ncol=1)
+  m<-model.matrix(formula, data)
+  y<-matrix(data[,as.character(formula)[2]],ncol=1)
   y<-y[as.numeric(rownames(m)),,drop=FALSE]
   
   #Fit via svd
@@ -23,7 +23,7 @@ ridge_reg<-function(form, lambda, data){
   D<-diag(svals/(svals^2 +lambda))
   beta<-V %*% D %*% t(U) %*% y
   rownames(beta) = colnames(m)
-  ret<- list(coefficients = beta, lambda=lambda,form=form)
+  ret<- list(coefficients = beta, lambda=lambda,formula=formula)
   class(ret)<-"ridge_reg"
   ret
 }#function end

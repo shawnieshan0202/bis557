c86ce7dbb76fb253ff99721329e99c82021ceb52
Xiangyi Shan <shawnieshan@Xiangyis-MBP.wireless.yale.internal>
2018-10-14 11:47:52 -0400

    homework2

--- R/ridge_model.R
+++ R/ridge_model.R
@@ -7,14 +7,6 @@
 #' @return An ridge regression object
 #' @export
 
-#read data files
-#ridge_train<-read.csv("/Users/shawnieshan/Desktop/ridge_train.csv")
-#ridge_test<-read.csv("/Users/shawnieshan/Desktop/ridge_test.csv")
-
-#library(MASS)
-#library(faraway)
-#library(stats)
-
 #build ridge function
 ridge_reg<-function(form, lambda, data){
   rownames(data) = NULL
@@ -22,18 +14,18 @@ ridge_reg<-function(form, lambda, data){
   y<-matrix(data[,as.character(form)[2]],ncol=1)
   y<-y[as.numeric(rownames(m)),,drop=FALSE]
   
-#Fit via svd
-svd_obj<-svd(m)
-U<-svd_obj$u
-V<-svd_obj$v
-svals<-svd_obj$d
-
-D<-diag(svals/(svals^2 +lambda))
-beta<-V %*% D %*% t(U) %*% y
-rownames(beta) = colnames(m)
-ret<- list(coefficients = beta, lambda=lambda,form=form)
-class(ret)<-"ridge_reg"
-ret
+  #Fit via svd
+  svd_obj<-svd(m)
+  U<-svd_obj$u
+  V<-svd_obj$v
+  svals<-svd_obj$d
+  
+  D<-diag(svals/(svals^2 +lambda))
+  beta<-V %*% D %*% t(U) %*% y
+  rownames(beta) = colnames(m)
+  ret<- list(coefficients = beta, lambda=lambda,form=form)
+  class(ret)<-"ridge_reg"
+  ret
 }#function end
 
 
